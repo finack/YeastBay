9f298b0d38b5483c93a04dd39cda2b1b42d23d6b
Peter Baker <peter@finack.com>
2011-04-23 23:58:01 -0700

    Skeleton version of Nanoc3/Compass/Haml/Sass site

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'compass'
+gem 'nanoc3'#, :git => 'https://github.com/ddfreyne/nanoc.git'
+#gem 'asdf'
---
+++ lib/helpers.rb
@@ -0,0 +1,35 @@
+## Stolen from https://github.com/unthinkingly/unthinkingly-blog/blob/master/lib/helpers.rb
+# Hyphens are converted to sub-directories in the output folder.
+#
+# If a file has two extensions like Rails naming conventions, then the first extension
+# is used as part of the output file.
+#
+#   sitemap.xml.erb # => sitemap.xml
+#
+# If the output file does not end with an .html extension, item[:layout] is set to 'none'
+# bypassing the use of layouts.
+#
+def route_path(item)
+  # in-memory items have not file
+  return item.identifier + "index.html" if item[:content_filename].nil?
+
+  url = item[:content_filename].gsub(/^content/, '')
+
+  # determine output extension
+  extname = '.' + item[:extension].split('.').last
+  outext = '.haml'
+  if url.match(/(\.[a-zA-Z0-9]+){2}$/) # => *.html.erb, *.html.md ...
+    outext = '' # remove 2nd extension
+  elsif extname == ".sass"
+    outext = '.css'
+  else
+    outext = '.html'
+  end
+  url.gsub!(extname, outext)
+
+  if url.include?('-')
+    url = url.split('-').join('/')  # /2010/01/01-some_title.html -> /2010/01/01/some_title.html
+  end
+
+  url
+end
